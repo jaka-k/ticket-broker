204c258dff69ddf1adafeeabd710f8f418be4532
jakak <74316018+jaka-k@users.noreply.github.com>
2024-04-23 22:30:54 +0200

    add create and update methods to order store

--- packages/core/src/lib/store.ts
+++ packages/core/src/lib/store.ts
@@ -1,15 +1,49 @@
 import { writable } from "svelte/store";
 
+type Status = 'pending' | 'success' | 'rejected';
+
 type Order = {
   id: string;
   countryCode: string;
   orderAmount: number;
-  status: 'pending' | 'success' | 'rejected';
+  status: Status
 };
 
+const { subscribe, set, update } = writable<Order[]>([]);
+
+function createOrder(id: string, countryCode: string, orderAmount: number, status: Order['status']): Order {
+  update((currentOrders: Order[]) => {
+    const newOrder: Order = { id, countryCode, orderAmount, status }
+    return [...currentOrders, newOrder]
+})
+}
+
+export function updateOrderStatus(orderID: string, newStatus: Status) {
+  update(allOrders => {
+    const index: number = allOrders.findIndex(order => order.id === orderID);
+    const order = allOrders[index];
+
+    if (!order) {
+      console.warn('Order not found for updating:', orderID);
+      return allOrders;
+    }
+
+    if (index !== -1) {
+      const updatedOrder: Order = {
+        id: order.id,
+        countryCode: order.countryCode,
+        orderAmount: order.orderAmount,
+        status: newStatus
+      };
+      return allOrders.map((order, idx) => idx === index ? updatedOrder : order);
+    }
+    return allOrders;
+  });
+}
+
+
 function createOrderStore() {
-    const { subscribe, set, update } = writable([]);
-  
+
     return {
       subscribe,
       logApiCall: (order: Order) => update((orders) => [...orders, order]),
